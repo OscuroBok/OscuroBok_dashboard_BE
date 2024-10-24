e5b2b4598c8db9a3f44c9915694b5f247e5f4af2
Ashish <shawashis500@gmail.com>
2024-10-24 22:35:57 +0530

    conflict resolved

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-const admin = require("./admin");
-const user = require("./user.route");
-const restaurant = require("./restaurant.route");
-const integrated = require("./integrated.route");
-=======
 /* This module sets up the main routes for version v1 of the API. 
    It imports the routes for users, restaurants and login features 
    and adds them to the server. The module exports the name and version 
@@ -14,7 +8,6 @@ const user = require("./v1/user.route");
 const restaurant = require("./v1/restaurant.route");
 const admin = require("./v1/admin.route");
 const integrated = require("./v1/integrated.route");
->>>>>>> ac7d63706f79cf92b18f19238cacfaebcf151dda
 
 module.exports = {
 	name: "base-route-v1",
